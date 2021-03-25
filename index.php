b4cf7763bb8d4ebaff3637cefd25707b3a5dd5f7
Oleksandr Omelchuk <realbattletoad@gmail.com>
2021-03-25 22:21:04 +0200

    cleanups

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        #canvas {
+            position: absolute;
+            top: 0; left: 0;
+            width: 100%;
+            height: 100%;
+            background: black;
+        }
+    </style>
+</head>
+<body>
+<canvas id="canvas"></canvas>
+<script type="module" src="./index.js"></script>
+</body>
+</html>
